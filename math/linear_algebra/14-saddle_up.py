4dbcc9f2128a8f24235e7b6e6f9e4fa3878061aa
semaed <figsaned@gmail.com>
2023-12-04 12:50:28 -0400

    Saddle Up

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    Module content:
+        - np_matmul: Matrix multiplication
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+        np_matmul - Matrix multiplication
+
+        @mat1: First matrix
+        @mat2: Second matrix
+
+        Return: New matrix with result of multiplication
+    """
+    return np.matmul(mat1, mat2)
