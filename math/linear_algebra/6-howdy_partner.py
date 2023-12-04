602020b1f8ee7804b7fe7e5828ee41d0d386fd4a
semaed <figsaned@gmail.com>
2023-12-04 12:23:55 -0400

    Howdy Partner

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Module content:
+        - cat_arrays - Concatenates two arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+        Concatenates two arrays
+            Args:
+                arr1: first array
+                arr2: second array
+            Return:
+                new array with arr1 and arr2 concatenated
+    """
+    return arr1 + arr2
