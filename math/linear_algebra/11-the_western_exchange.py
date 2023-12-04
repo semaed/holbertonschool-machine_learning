4dbbb4c0700099970d7a6a2e1cb73ae758a64229
semaed <figsaned@gmail.com>
2023-12-04 12:42:04 -0400

    The Western Exchange

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Module content:
+        - np_transpose: Transpose a matrix
+"""
+
+
+def np_transpose(matrix):
+    """
+        Compute the transpose of a numpy.ndarray"""
+    return matrix.transpose()
