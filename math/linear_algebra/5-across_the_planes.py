41ff1aa6f814cf25ee4e8c70501ba51bae88f3f1
semaed <figsaned@gmail.com>
2023-12-04 12:21:30 -0400

    Correction the code

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 """
-    Module Adds two matrices element-wise and Calculates the shape of a matrix
+    Module content:
+        - add_matrices2D - Adds two matrices element-wise
+        - matrix_shape - Calculates the shape of a matrix
 """
 
 
 def add_matrices2D(mat1, mat2):
     """
-        Adds two matrices element-wise
-            Args:
-                mat1: first matrix
-                mat2: second matrix
-            Return:
-                new matrix with sum of mat1 and mat2
+        add_matrices2D - Adds two matrices element-wise
+
+        @mat1 & @mat2: Matrices to be added
+
+        Return: Upon success returns a new matrix with the sum,
+                otherwise None
+
+        Description: In this function, there are two ways that will
+        render the same result.
     """
-
-
-def add_matrices2D(mat1, mat2):
     if len(mat1) != len(mat2):
         return None
 
