62e68322bfc58b4147b90fd10ec3800067dfe6d6
semaed <figsaned@gmail.com>
2023-12-04 12:38:26 -0400

    I’ll Use My Scale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    Module content:
+        - np_shape: Calculates the shape of a numpy.ndarray
+"""
+
+
+def np_shape(matrix):
+    """
+        np_shape: Calculates the shape of a numpy.ndarray
+
+        @matrix: Numpy array to calculate shape of
+
+        Return: Tuple of integers representing shape of @matrix
+    """
+    return matrix.shape
