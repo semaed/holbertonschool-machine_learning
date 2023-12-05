d52ad2d688ce128465453c9528b2755d9cb538b7
semaed <figsaned@gmail.com>
2023-12-05 08:58:58 -0400

    Correction to the code

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 """Module Concatenates two matrices along a specific axis"""
 
-import copy
-
 
 def cat_matrices(mat1, mat2, axis=0):
     """Concatenates two matrices along a specific axis"""
 
+    # Convert 1D lists to 2D lists
+    if isinstance(mat1[0], int):
+        mat1 = [[item] for item in mat1]
+    if isinstance(mat2[0], int):
+        mat2 = [[item] for item in mat2]
+
     # Create deep copies of the input matrices
-    mat1 = copy.deepcopy(mat1)
-    mat2 = copy.deepcopy(mat2)
+    mat1 = [row[:] for row in mat1]
+    mat2 = [row[:] for row in mat2]
 
     # Check if mat1 and mat2 are lists of lists (2D matrices)
     if (not all(isinstance(row, list) for row in mat1) or
