9d0eda8f90b7580d7ac92acf1f10ba96eb61969b
semaed <figsaned@gmail.com>
2023-12-05 09:01:18 -0400

    Correction to the code

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -11,27 +11,21 @@ def cat_matrices(mat1, mat2, axis=0):
     if isinstance(mat2[0], int):
         mat2 = [[item] for item in mat2]
 
-    # Create deep copies of the input matrices
-    mat1 = [row[:] for row in mat1]
-    mat2 = [row[:] for row in mat2]
-
     # Check if mat1 and mat2 are lists of lists (2D matrices)
     if (not all(isinstance(row, list) for row in mat1) or
             not all(isinstance(row, list) for row in mat2)):
         return None
 
-    # Concatenate along axis 0
-    if axis == 0:
-        if len(mat1[0]) != len(mat2[0]):
-            return None
-        return [row[:] for row in mat1] + [row[:] for row in mat2]
-
-    # Concatenate along axis 1
-    elif axis == 1:
-        if len(mat1) != len(mat2):
-            return None
-        return [mat1[i][:] + mat2[i][:] for i in range(len(mat1))]
-
-    # If axis is neither 0 nor 1, return None
-    else:
+    # Check if the dimensions of the matrices match
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
+
+    # Recursive function to concatenate matrices along a specific axis
+    def concat_recursive(m1, m2, ax):
+        if ax == 0:
+            return m1 + m2
+        else:
+            return [concat_recursive(m1[i], m2[i], ax-1) for i in range(len(m1))]
+
+    # Call the recursive function
+    return concat_recursive(mat1, mat2, axis)
