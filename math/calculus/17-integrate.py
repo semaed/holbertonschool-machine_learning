5cd59d7b40fc5043af5a78dfedff7ad5d8dde821
semaed <figsaned@gmail.com>
2023-12-14 11:06:23 -0400

    Integrate

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+    Module Calculate the integral of a polynomial
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+        Calculates the integral of a polynomial
+        poly: list of coefficients representing a polynomial
+        C: is an integer representing the integration constant
+        Return: a new list of coefficients representing the
+        integral of the polynomial
+    """
+    if type(poly) is not list or len(poly) == 0:
+        return None
+    if type(C) is not int:
+        return None
+    if len(poly) == 1:
+        return [C]
+    integral = [C]
+    for i in range(len(poly)):
+        if type(poly[i]) is not int and type(poly[i]) is not float:
+            return None
+        integral.append(poly[i] / (i + 1))
+    return integral
