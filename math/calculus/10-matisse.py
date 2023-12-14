00e6a0b0c4be4531fd6334b6fcbd677e3210afe0
semaed <figsaned@gmail.com>
2023-12-14 11:02:30 -0400

    Derive happiness in oneself from a good day's work

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+    Module content:
+        - poly_derivative: Calculate the derivative of a
+        polynomial
+"""
+
+
+def poly_derivative(poly):
+    """
+        Calculates the derivative of a polynomial
+        poly: list of coefficients representing a polynomial
+        Return: a new list of coefficients representing the
+        derivative of the polynomial
+    """
+    if type(poly) is not list or len(poly) == 0:
+        return None
+    if len(poly) == 1:
+        return [0]
+    derivative = []
+    for i in range(1, len(poly)):
+        if type(poly[i]) is not int and type(poly[i]) is not float:
+            return None
+        derivative.append(i * poly[i])
+    return derivative
