4a9dd08ce0c47e89c629d77b1ec77f873df59571
semaed <figsaned@gmail.com>
2023-12-19 11:10:07 -0400

    Initialize Normal

---
+++ math/probability/normal.py
@@ -0,0 +1,39 @@
+# usr/bin/env python3
+"""Create a class Normal that represents a normal distribution"""
+
+
+class Normal:
+    """class that represents normal distribution"""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        class constructor
+
+        parameters:
+            data [list]: data to be used to estimate the distibution
+            mean [float]: the mean of the distribution
+            stddev [float]: the standard deviation of the distribution
+
+        Sets the instance attributes mean and stddev as floats
+        If data is not given:
+            Use the given mean and stddev
+            raise ValueError if stddev is not positive value
+        If data is given:
+            Calculate the mean and stddev of data
+            Raise TypeError if data is not a list
+            Raise ValueError if data does not contain at least two data points
+        """
+        self.data = data
+        self.mean = float(mean)
+        self.stddev = float(stddev)
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            self.stddev = (sum([(i - self.mean)**2 for i in data]) /
+                           len(data)) ** 0.5
