54405bf89001ef8d13a74457d7c2a44fe064619d
semaed <figsaned@gmail.com>
2024-01-08 21:00:43 -0400

    Privatize DeepNeuralNetwork

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Defines a deep neural network performing binary classification
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+        Defines a deep neural network performing
+        binary classification
+    """
+
+    def __init__(self, nx, layers):
+        """
+            Class constructor
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for layer in range(self.__L):
+            if type(layers[layer]) is not int or layers[layer] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            self.__weights['b' + str(layer + 1)] = np.zeros((layers[layer], 1))
+            if layer == 0:
+                He_et_al = np.random.randn(layers[layer], nx) * np.sqrt(2/nx)
+                self.__weights['W' + str(layer + 1)] = He_et_al
+            else:
+                He_et_al = np.random.randn(layers[layer], layers[layer - 1]) *\
+                    np.sqrt(2/layers[layer-1])
+                self.__weights['W' + str(layer + 1)] = He_et_al
+
+    @property
+    def L(self):
+        """ Getter function """
+        return self.__L
+
+    @property
+    def cache(self):
+        """ Getter function """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """ Getter function """
+        return self.__weights
