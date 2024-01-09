ee37f258af7c58e5236dc6f61ec22466c12508e1
semaed <figsaned@gmail.com>
2024-01-08 20:18:00 -0400

    Privatize NeuralNetwork

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""Neuron class that defines a single neuron performing binary classification
+"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+        NeuralNetwork with one hidden layer performing binary
+        classification
+    """
+
+    def __init__(self, nx, nodes):
+        """
+            __init__: class constructor
+
+            Input:
+                @nx: the number of input features
+                @nodes: the number of nodes found in the hidden layer
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be a integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """
+            Getter function
+
+            Return: the weight vector for the hidden layer
+        """
+        return self.__W1
+
+    @property
+    def W2(self):
+        """
+            Getter function
+
+            Return: The weight vector for the output neuron
+        """
+        return self.__W2
+
+    @property
+    def b1(self):
+        """
+            Getter function
+
+            Return: the bias for the hidden layer
+        """
+        return self.__b1
+
+    @property
+    def b2(self):
+        """
+            Getter function
+
+            Return: the bias for the output neuron
+        """
+        return self.__b2
+
+    @property
+    def A1(self):
+        """
+            Getter function
+
+            Return: the activated output for the hidden layer
+        """
+        return self.__A1
+
+    @property
+    def A2(self):
+        """
+            Getter function
+
+            Return: the activated output fot the output neuron
+        """
+        return self.__A2
