68b273861104197fe5b358f602166e75a54a6d36
semaed <figsaned@gmail.com>
2024-01-28 16:35:18 -0400

    Gradient Descent with Dropout

---
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Defines function that updates the weights with Dropout regularization
+using gradient descent
+"""
+import numpy as np
+
+
+def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
+    """ Updates the weights with Dropout regularization using gradient descent"""
+    m = Y.shape[1]
+    dz = cache["A" + str(L)] - Y
+    for x in range(L, 0, -1):
+        A_prev = cache["A" + str(x - 1)]
+        dw = np.matmul(dz, A_prev.T) / m
+        db = np.sum(dz, axis=1, keepdims=True) / m
+        inv = 1 - np.square(A_prev)
+        da = np.matmul(weights["W" + str(x)].T, dz)
+        if x > 1:
+            da = da * cache["D" + str(x - 1)]
+            da = da / keep_prob
+        dz = da * inv
+        weights["W" + str(x)] -= alpha * dw
+        weights["b" + str(x)] -= alpha * db
