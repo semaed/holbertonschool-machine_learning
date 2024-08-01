44c713ddc441eebaf2cc162fe128bb3354d94e48
semaed <figsaned@gmail.com>
2024-08-01 11:21:00 -0400

    Can I join?

---
+++ pipeline/apis/0-passengers.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" Get requests"""
+import requests
+
+
+def availableShips(passengerCount):
+    """
+    Function to get request
+    Args:
+        passengerCount: number of passangers
+    Returns: List of starships
+    """
+    starships = []
+    url = 'https://swapi-api.hbtn.io/api/starships/'
+    while url is not None:
+        response = requests.get(url,
+                                headers={'Accept': 'application/json'},
+                                params={"term": 'starships'})
+        for ship in response.json()['results']:
+            passenger = ship['passengers'].replace(',', '')
+            if passenger.isnumeric() and int(passenger) >= passengerCount:
+                starships.append(ship['name'])
+        url = response.json()['next']
+    return starships
